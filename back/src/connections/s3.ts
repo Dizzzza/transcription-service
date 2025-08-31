6119b2f373deab30d63f98f5d0d3b0aea93ee182
Dizzzza <dias.svankulov@gmail.com>
2025-09-01 00:04:22 +0500

    deleted unnecessary console.log and added .env.example;

--- back/src/connections/s3.ts
+++ back/src/connections/s3.ts
@@ -1,8 +1,5 @@
 import { S3Client } from '@aws-sdk/client-s3';
 
-console.log(process.env.MINIO_ROOT_USER);
-console.log(process.env.MINIO_ROOT_PASSWORD);
-
 export const s3 = new S3Client({
   region: process.env.MINIO_ROOT_REGION || 'us-east-1',
   endpoint: process.env.MINIO_ENDPOINT || 'http://localhost:9000',
